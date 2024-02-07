a60b53f4a8eb5052cdf43435483be4c0175856f0
Anton Pavliuk <streetreadger@gmail.com>
2024-02-07 23:13:55 +0200

    Add gtk dependencie and demo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow};
+
 fn main() {
-    println!("Hello, world!");
+    let app = Application::new(
+        Some("com.shinglyu.catsay-gui"),
+        Default::default()
+    );
+
+    app.connect_activate(|app| {
+        let window = ApplicationWindow::new(app);
+        window.set_title("Catsay");
+        window.set_default_size(350, 70);
+
+        window.show_all();
+    });
+
+    app.run();
 }
